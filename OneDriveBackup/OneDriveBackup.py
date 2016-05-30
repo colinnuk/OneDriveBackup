3977dfdf86313ad5e382a5febf1e4506a629dd94
Colin Noakes <colin.noakes@outlook.com>
2016-05-30 22:03:00 +0100

    Add settings handler and build the copy structure & commands

--- OneDriveBackup/OneDriveBackup.py
+++ OneDriveBackup/OneDriveBackup.py
@@ -16,21 +16,57 @@ def main():
     client = auth()
 
     setup(client)
+    settings = get_settings()
+
+    copy_folders(client, settings['BackupFolders'])
+    
+    sys.exit()
+
+def copy_folders(client, backup_folders):
+    split_folders = backup_folders.split(',')
+    for folder_id in split_folders:
+        copy_folder(client, folder_id)
 
 
-def copy_folder(client):
-    logging.info('Start copy')
-    ref = onedrivesdk.ItemReference()
-    ref.id = 'EC7CE424F7CB9079!23588'
-    copy_operation = client.item(drive="me", id="EC7CE424F7CB9079!19151").copy(name="USAPicsTest", parent_reference=ref).post() # USA Pics
-    copy_operation._completed = True # Need to use this rather than _stop_poll_on_thread() until https://github.com/OneDrive/onedrive-sdk-python/pull/31 is merged
+def copy_folder(client, folder_id):
+    if folder_id == '':
+        return
+    
+    try:
+        top_level_backup_folder = client.item(drive='me', path='OneDriveBackup/' + folder_id).get()
+    except onedrivesdk.error.OneDriveError as e:
+        if e.code == 'itemNotFound':
+            # create the top level backup folder if it was not found - date level folders will be stored in here
+            new_folder = onedrivesdk.Folder()
+            new_folder_item = onedrivesdk.Item()
+            new_folder_item.name = folder_id
+            new_folder_item.folder = new_folder
+            top_level_backup_folder = client.item(drive='me', path='OneDriveBackup').children.add(new_folder_item)
+
+    logging.info('Start copy for folder id ' + folder_id + ' into folder id ' + top_level_backup_folder.id + ' as ' + time.strftime('%Y%m%d_%H%M', time.gmtime()))
+    #copy_operation = client.item(drive='me', id=folder_id).copy(name=time.strftime('%Y%m%d_%H%M', time.gmtime()), parent_reference=backup_folder).post()
+    #copy_operation._completed = True # Need to use this rather than _stop_poll_on_thread() until https://github.com/OneDrive/onedrive-sdk-python/pull/31 is merged
+
+def get_settings():
+    settings = {}
+    lines = [line.strip() for line in open(get_settings_path())]
+    for l in lines:
+        split_line = l.split('=')
+        settings[split_line[0]] = split_line[1]
+
+    #if the settings file doesn't contain the below items we need to quit
+    if 'BackupFolders' not in settings:
+        logging.critical('Invalid settings file; should contain "BackupFolders" key')
+        sys.exit()
+
+    return settings
 
 def setup(client):
     # try and download our settings file /OneDriveBackup/settings to our app storage, if it doesn't exist then create and upload it
-    onedrivebackup_folder = client.item(drive="me", path="OneDriveBackup").children.get()
+    onedrivebackup_folder = client.item(drive='me', path='OneDriveBackup').children.get()
     for item in onedrivebackup_folder:
-        if item.name == "settings":
-            client.item(drive="me", id=item.id).download(get_settings_path())
+        if item.name == 'settings':
+            client.item(drive='me', id=item.id).download(get_settings_path())
 
     if not os.path.isfile(get_settings_path()):
         edit_settings(client)
@@ -41,12 +77,12 @@ def edit_settings(client):
         # get all the root's children so user can select which folders to backup - ignore the 'OneDriveBackup' folder
         folders = client.item(drive='me', id='root').children.get()
         for folder in folders:
-            if folder.name != "OneDriveBackup" and input('Do you want to include folder "' + folder.name + '" in the backup? Yes=Y\t') == 'Y':
+            if folder.name != 'OneDriveBackup' and input('Do you want to include folder "' + folder.name + '" in the backup? Yes=Y\t') == 'Y':
                 settings.write(folder.id + ',')
 
         settings.write('\nBackupsToKeep=' + input('How many backups do you want to keep?\t'))
         settings.close()
-        uploaded_item = client.item(drive="me", path="OneDriveBackup").children["settings"].upload(get_settings_path())
+        uploaded_item = client.item(drive='me', path='OneDriveBackup').children['settings'].upload(get_settings_path())
         logging.info('Updated & uploaded settings')
     
 
